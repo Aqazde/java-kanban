0e8db339df88211519a107eb351193810ad2f555
Aqazde <adil64489@gmail.com>
2024-10-12 20:20:47 +0500

    check for null value

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -10,6 +10,9 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void add(Task task) {
+        if (task == null) {
+            return;
+        }
         history.add(task);
         if (history.size() > 10) {
             history.remove(0);

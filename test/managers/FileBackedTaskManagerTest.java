9b4b501f5b5db8a55cd3acc83446534fa107d622
Aqazde <adil64489@gmail.com>
2025-03-06 10:15:05 +0500

    modify: test-cases in 1 test

--- test/managers/FileBackedTaskManagerTest.java
+++ test/managers/FileBackedTaskManagerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tasks.Epic;
 import tasks.Task;
+import tasks.Subtask;
+import tasks.Status;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,22 +23,36 @@ class FileBackedTaskManagerTest {
     }
 
     @Test
-    void testSaveAndLoadEmptyFile() {
-        FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(tempFile);
-        assertTrue(loadedManager.getAllTasks().isEmpty(), "Загруженный менеджер должен быть пустым");
-    }
+    void testSaveAndLoadFromFile() {
+        Task task = new Task("Учеба", "Написать дипломную работу");
+        fileManager.createTask(task);
 
-    @Test
-    void testSaveAndLoadMultipleTasks() {
-        Task task1 = new Task("Учеба", "Закончить курс");
-        fileManager.createTask(task1);
+        Epic epic = new Epic("Диплом", "Создать приложение и написать отчет");
+        fileManager.createEpic(epic);
 
-        Epic epic1 = new Epic("Проект", "Создать приложение");
-        fileManager.createEpic(epic1);
+        Subtask subtask = new Subtask("Выбрать модель", "Сравнить модели", epic.getId());
+        fileManager.createSubtask(subtask);
 
         FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(tempFile);
 
         assertEquals(1, loadedManager.getAllTasks().size(), "Должна быть одна сохраненная задача");
         assertEquals(1, loadedManager.getAllEpics().size(), "Должен быть один сохраненный эпик");
+        assertEquals(1, loadedManager.getAllSubtasks().size(), "Должна быть одна сохраненная " +
+                "подзадача");
+
+        assertEquals(task, loadedManager.getAllTasks().get(0), "Задачи не совпадают");
+        assertEquals(epic, loadedManager.getAllEpics().get(0), "Эпики не совпадают");
+        assertEquals(subtask, loadedManager.getAllSubtasks().get(0), "Подзадачи не совпадают");
+
+        task.setStatus(Status.DONE);
+        fileManager.updateTask(task);
+
+        FileBackedTaskManager updatedManager = FileBackedTaskManager.loadFromFile(tempFile);
+        assertEquals(Status.DONE, updatedManager.getTaskById(task.getId()).getStatus(), "Статус задачи " +
+                "должен обновиться");
+
+        fileManager.deleteTask(task.getId());
+        FileBackedTaskManager afterDeleteManager = FileBackedTaskManager.loadFromFile(tempFile);
+        assertTrue(afterDeleteManager.getAllTasks().isEmpty(), "Задачи должны быть удалены");
     }
 }

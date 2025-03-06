9a881e6049874ec020436844e108054b788b88e6
Aqazde <adil64489@gmail.com>
2025-03-06 09:51:15 +0500

    add: new method linked with TaskType

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -51,6 +51,11 @@ public class Epic extends Task {
     }
 
     @Override
+    public TaskType getType() {
+        return TaskType.EPIC;
+    }
+
+    @Override
     public String toString() {
         return "Tasks.Epic{" +
                 "id=" + getId() +

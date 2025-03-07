8b3117e5150f71886e1966a09081cd7422b731f9
Aqazde <adil64489@gmail.com>
2025-03-07 14:47:52 +0500

    fixed: separators for checkStyle

--- src/managers/InMemoryTaskManager.java
+++ src/managers/InMemoryTaskManager.java
@@ -188,12 +188,14 @@ public class InMemoryTaskManager implements TaskManager {
         historyManager.add(task);
         return task;
     }
+
     @Override
     public Epic getEpicById(int id) {
         Epic epic = epics.get(id);
         historyManager.add(epic);
         return epic;
     }
+
     @Override
     public Subtask getSubtaskById(int id) {
         Subtask subtask = subtasks.get(id);

b18d0dd26b41ac3c0188e81dfddc050288513fb4
Aqazde <adil64489@gmail.com>
2025-03-07 12:39:48 +0500

    modify: serialization and deserialization

--- src/utils/TaskConverter.java
+++ src/utils/TaskConverter.java
@@ -2,16 +2,26 @@ package utils;
 
 import tasks.*;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class TaskConverter {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public static String toString(Task task) {
         String epicId = (task instanceof Subtask) ? String.valueOf(((Subtask) task).getEpicId()) : "";
-        return String.format("%d,%s,%s,%s,%s,%s",
+        String startTime = (task.getStartTime() != null) ? task.getStartTime().format(FORMATTER) : "";
+        String duration = String.valueOf(task.getDuration().toMinutes());
+
+        return String.format("%d,%s,%s,%s,%s,%s,%s,%s",
                 task.getId(),
                 task.getType(), // Для упрощения создан новый метод, связанный с Task (убрано instanceof)
                 task.getTitle(),
                 task.getStatus(),
                 task.getDescription(),
+                duration,
+                startTime,
                 epicId
         );
     }
@@ -23,8 +33,10 @@ public class TaskConverter {
         String name = parts[2];
         Status status = Status.valueOf(parts[3]);
         String description = parts[4];
-
+        Duration duration = Duration.ofMinutes(Long.parseLong(parts[5]));
+        LocalDateTime startTime = parts[6].isEmpty() ? null : LocalDateTime.parse(parts[6], FORMATTER);
         Task task;
+
         switch (type) {
             case TASK:
                 task = new Task(name, description);
@@ -33,7 +45,7 @@ public class TaskConverter {
                 task = new Epic(name, description);
                 break;
             case SUBTASK:
-                int epicId = Integer.parseInt(parts[5]);
+                int epicId = Integer.parseInt(parts[7]);
                 task = new Subtask(name, description, epicId);
                 break;
             default:
@@ -41,6 +53,8 @@ public class TaskConverter {
         }
         task.setId(id);
         task.setStatus(status);
+        task.setDuration(duration);
+        task.setStartTime(startTime);
         return task;
     }
 }

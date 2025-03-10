e251420379bfe514a0567d1aa45ac6a2e5b4f37a
Aqazde <adil64489@gmail.com>
2025-03-10 16:01:18 +0500

    fixed: codeStyle

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Comparator;
 
 public class Epic extends Task {
     private List<Subtask> subtasks;

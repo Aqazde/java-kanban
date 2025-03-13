a084aabcef69407bd120a09ba6a444a8b3f953e8
Aqazde <adil64489@gmail.com>
2025-03-13 10:52:39 +0500

    modify: duration and dateTime adapters

--- src/http/BaseHttpHandler.java
+++ src/http/BaseHttpHandler.java
@@ -1,16 +1,24 @@
 package http;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import utils.DurationTypeAdapter;
+import utils.LocalDateTimeTypeAdapter;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public abstract class BaseHttpHandler implements HttpHandler {
 
-    protected final Gson gson = new Gson();
+    protected final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(Duration.class, new DurationTypeAdapter())
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
+            .create();
 
     protected void sendText(HttpExchange exchange, String text, int statusCode) throws IOException {
         byte[] responseBytes = text.getBytes(StandardCharsets.UTF_8);
